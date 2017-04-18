9e62cba24bbbe305c3303726111ec555b5472f43
Breana Knight <Akewlbreeze.bc@gmail.com>
2017-04-18 08:27:24 -0600

    tests written for main application features

--- test/current.test.js
+++ test/current.test.js
@@ -1,1 +1,17 @@
+import React from 'react'
+import {shallow , mount } from 'enzyme'
+import WeatherAPI from './WeatherAPI.js';
+import Main from '../lib/components/Main.js'
+import Current from '../lib/components/Current.js';
+import Input from '../lib/components/Input.js'
 
+describe('state.input should change when input is changed', () => {
+  let wrapper = shallow(<Input fetchWeather ={()=>{}} />);
+  it('Should do something', () => {
+    let button = wrapper.find('.submit-button');
+    let input = wrapper.find('.location-input[type="text"]')
+    input.simulate('change', { target: { value: 'Denver, CO'}});
+    button.simulate('click');
+    expect(wrapper.state('input')).toEqual('Denver, CO')
+  })
+})
